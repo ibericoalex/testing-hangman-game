4ce903dc1d6663aea84d2a6a900f93c2bafb4e05
Your Name <you@example.com>
2023-09-08 16:32:06 +0200

    Generate random word and convert to uppercase

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
 import random
+from words import words
 
-def select_word(words):
-    return random.choice(words)
 
-def print_secret_word(secret_word):
-    print(" _ " * len(secret_word))
 
-words = ["tiger", "three", "tree", "abrupt", "acclaim", "adhere", "advice", "afflict", "agitate", "allocate", "ambition", "amplify", "anecdote"]
-remaining_attempts = 6
-guessed_letter = ""
+def generate_random_word(words):
+    """
+    Function to select a random word from the list and
+    return the letter in uppercase.
+    """
+    word = random.choice(words)
+    return word.upper()
 
-print("Welcome to the Hangman Game! \n")
-secret_word = select_word(words)
-print_secret_word(secret_word)
