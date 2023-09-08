c25a2a5481a8e80ba61f72aaddefbd07d4fe50b1
Your Name <you@example.com>
2023-09-08 15:15:47 +0200

    Create hangman stages

--- run.py
+++ run.py
@@ -4,5 +4,7 @@ def select_word(words):
     return random.choice(words)
 
 words = ["tiger", "three", "tree", "abrupt", "acclaim", "adhere", "advice", "afflict", "agitate", "allocate", "ambition", "amplify", "anecdote"]
+remaining_attempts = 6
+guessed_letter = ""
 
-print(select-word(words))
+print(select_word(words))
