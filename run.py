85aa9657ebf8e95b00dcbcbf9cc3e1c12b660289
Your Name <you@example.com>
2023-09-08 15:55:01 +0200

    Add 200 words for the game

--- run.py
+++ run.py
@@ -3,8 +3,13 @@ import random
 def select_word(words):
     return random.choice(words)
 
+def print_secret_word(secret_word):
+    print(" _ " * len(secret_word))
+
 words = ["tiger", "three", "tree", "abrupt", "acclaim", "adhere", "advice", "afflict", "agitate", "allocate", "ambition", "amplify", "anecdote"]
 remaining_attempts = 6
 guessed_letter = ""
 
-print(select_word(words))
+print("Welcome to the Hangman Game! \n")
+secret_word = select_word(words)
+print_secret_word(secret_word)
